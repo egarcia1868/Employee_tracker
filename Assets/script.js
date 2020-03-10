8927713aabdf4b170ec67fa4f0a98a24c834f9de
Eric Garcia <egarcia1868@gmail.com>
2020-03-09 20:14:56 -0500

    pulling and displaying employee info in the table correctly

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,14 @@
 //  need a require thing here to bring in the table info for manager info
 
 class Employee {
-  constructor(id, first_name, last_name, title, salary) {
+  constructor(id, first_name, last_name, title, department, salary, manager) {
     this.id = id;
     this.first_name = first_name;
     this.last_name = last_name;
     this.title = title;
-    this.department;
+    this.department = department;
     this.salary = salary;
-    this.manager;
+    this.manager = manager;
 
   }
 //   getFirstName() {
--- trackerReadData.js
+++ trackerReadData.js
@@ -31,25 +31,57 @@ function displayAll() {
   let formattedRes = [];
   connection.query(`SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, role.title, role.salary, employee.manager_id FROM employee INNER JOIN role ON employee.role_id=role.id`, (err, res1) => {
     if (err) throw err;
-    // console.log(res1[4].manager_id);
     res1.map(emp => {
-      const genEmp = new Employee(emp.id, emp.first_name, emp.last_name, emp.title, emp.salary);
+      const genEmp = new Employee(emp.id, emp.first_name, emp.last_name, emp.title, null, emp.salary, null);
+      connection.query(`SELECT department.name FROM role INNER JOIN department ON role.department_id=department.id AND role.id=${emp.role_id}`, (err, res2) => {
+        if (err) throw err;
+        // console.log("dep: "+ res2[0].name)
+        genEmp.department = res2[0].name;
+        // console.log(genEmp);
+      });
       connection.query(`SELECT employee.first_name, employee.last_name FROM employee WHERE employee.id=${emp.manager_id}`, (err, res2) => {
         if (err) throw err;
         if (res2.length > 0) {
         // console.log(res2[0].first_name)
         genEmp.manager = res2[0].first_name+" "+res2[0].last_name;
         }
-      })
-      connection.query(`SELECT department.name FROM role INNER JOIN department ON role.department_id=department.id AND role.id=${emp.role_id}`, (err, res2) => {
-        if (err) throw err;
-        // console.log("dep: "+ res2[0].name)
-        genEmp.department = res2[0].name;
-        console.log(genEmp);
-      })
-      formattedRes.push(genEmp);
-    })
-    console.log(formattedRes);  
+        formattedRes.push(genEmp);
+        if (res1.length === formattedRes.length) {
+          formattedRes.sort((a, b) => {return a.id - b.id});
+          console.table(formattedRes);
+          };
+      });
+      // console.log(`${counter} || ${res1.length} `)
+
+    });
+    // console.log(res1[4].manager_id);
+    // res1.map(emp => {
+    //   counter++;
+    //   const genEmp = new Employee(emp.id, emp.first_name, emp.last_name, emp.title, emp.salary);
+    //   console.log(`counterinside0: ${counter}`);
+    //   connection.query(`SELECT employee.first_name, employee.last_name FROM employee WHERE employee.id=${emp.manager_id}`, (err, res2) => {
+    //     if (err) throw err;
+    //     if (res2.length > 0) {
+    //     // console.log(res2[0].first_name)
+    //     genEmp.manager = res2[0].first_name+" "+res2[0].last_name;
+    //     console.log(`counterinside1: ${counter}`);
+    //     }
+    //   })
+    //   console.log(`counterOUTSIDE: ${counter}`);
+    //   connection.query(`SELECT department.name FROM role INNER JOIN department ON role.department_id=department.id AND role.id=${emp.role_id}`, (err, res2) => {
+    //     if (err) throw err;
+    //     // console.log("dep: "+ res2[0].name)
+    //     genEmp.department = res2[0].name;
+    //     // console.log(genEmp);
+    //     formattedRes.push(genEmp);
+    //     console.log(`counterinside2: ${counter}`);
+    //   });
+    //   console.log(`counter:  ${counter} | res1.length:  ${res1.length}`);
+    //   if (counter === res1.length) {
+    //     console.log(formattedRes);
+    //     };
+    // });
+
     // console.log(res1);
 
     // SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, employee.manager_id 
@@ -80,6 +112,8 @@ function displayAll() {
 
     //   }
     // ]);
+    
   connection.end();
   });
+
 }
